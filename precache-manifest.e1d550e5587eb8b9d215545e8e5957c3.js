853b8d301f6e77676a11bee9cd0b3b60907c7de7
Nazar Tolstoy <nazar.tolstoy@topstack.dev>
2020-07-07 12:11:42 +0300

    Updates

---
+++ precache-manifest.e1d550e5587eb8b9d215545e8e5957c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6758f3057ddfc20a8bcb",
+    "url": "/react-hooks-todo-app-master/static/css/main.35e7710a.chunk.css"
+  },
+  {
+    "revision": "6758f3057ddfc20a8bcb",
+    "url": "/react-hooks-todo-app-master/static/js/main.6758f305.chunk.js"
+  },
+  {
+    "revision": "4e3baa7984ac939b34d2",
+    "url": "/react-hooks-todo-app-master/static/js/1.4e3baa79.chunk.js"
+  },
+  {
+    "revision": "b4722dd29cfe42edba4b",
+    "url": "/react-hooks-todo-app-master/static/js/runtime~main.b4722dd2.js"
+  },
+  {
+    "revision": "14494c765d85349bf7463463c50eaa28",
+    "url": "/react-hooks-todo-app-master/index.html"
+  }
+];
